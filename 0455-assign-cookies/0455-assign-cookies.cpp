0db3abd933afa565b1d3624c26427bafafa9f48f
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-05-22 23:56:03 +0530

    Time: 862 ms (5.01%), Space: 17.4 MB (69.49%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        for(int i=0;i<g.size();i++)
+        {
+            for(int j=0;j<s.size();j++)
+            {
+                if(s[j]>=g[i])
+                {
+                    ans++;
+                    s[j]=INT_MIN;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
