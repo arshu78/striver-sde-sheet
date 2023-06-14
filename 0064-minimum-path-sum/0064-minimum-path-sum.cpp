2f18ea2026af0d7cd5a24cffb4feb5c434430471
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-06-14 21:59:48 +0530

    Time: 7 ms (92.18%), Space: 10.1 MB (46.44%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -14,6 +14,24 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return f(n-1,m-1,grid,dp);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else
+                {
+                    int up=1e9,left=1e9;
+                    if(i>0)
+                        up=dp[i-1][j] + grid[i][j];
+                    
+                    if(j>0) 
+                        left=dp[i][j-1] + grid[i][j];
+                    
+                    dp[i][j]= min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
