034f8d1f5e2acd0d8fb5f009ca905a6d87d3af14
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-06-14 21:35:34 +0530

    Added solution - LeetHub

---
+++ Unique Paths in a Grid - GFG/unique-paths-in-a-grid.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+  int mod=1e9+7;
+    long long int f(long long int i,long long int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
+    {
+        if(i>=0 && j>=0 && grid[i][j]==0) return 0;
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        long long int up=f(i-1,j,grid,dp) % mod;
+        long long int left=f(i,j-1,grid,dp) % mod;
+        
+        return dp[i][j] = up+ left % mod;
+    }
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        return f(n-1,m-1,grid,dp) % mod;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m,x;
+        cin>>n>>m;
+        
+        vector<vector<int>> grid(n,vector<int>(m));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                cin>>grid[i][j];
+            }
+        }
+
+        Solution ob;
+        cout << ob.uniquePaths(n,m,grid) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
