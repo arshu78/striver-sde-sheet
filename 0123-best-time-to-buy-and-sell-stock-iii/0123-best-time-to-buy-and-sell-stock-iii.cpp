9b7bcdfc71e6f8e6768f1ca489407d4885a83275
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-07-06 14:43:24 +0530

    Time: 996 ms (5.02%), Space: 272.6 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -21,7 +21,31 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,2,prices,n,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,0)));
+        
+        dp[n][0][0]=dp[n][1][0]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=2;cap++)
+                {
+                     int profit=0;
+                    if(buy)
+                    {
+                        profit=max(-prices[i] + dp[i+1][0][cap],
+                                  0 + dp[i+1][1][cap]);
+                    }
+                    else
+                    {
+                        profit=max(prices[i] + dp[i+1][1][cap-1],
+                                  0 + dp[i+1][0][cap]);
+                    }
+                    dp[i][buy][cap]= profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
