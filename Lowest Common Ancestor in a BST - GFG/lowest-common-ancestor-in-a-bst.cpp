d62d183c7af333fcc0b05ac48022cddbab787d4e
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-07-12 19:53:05 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a BST - GFG/lowest-common-ancestor-in-a-bst.cpp
+++ Lowest Common Ancestor in a BST - GFG/lowest-common-ancestor-in-a-bst.cpp
@@ -21,18 +21,17 @@ class Solution{
     public:
         Node* LCA(Node *root, int n1, int n2)
         {
-            if(root==NULL || root->data==n1 || root->data==n2) return root;
-            
-            Node* left=LCA(root->left,n1,n2);
-            Node* right=LCA(root->right,n1,n2);
-            
-            if(left==NULL) return right;
-            else if(right==NULL) return left;
-            else{
-                //both are not null an it is an LCA
-                
-                return root;
+            while(root!=NULL) 
+            {
+                if(root->data<n1 && root->data<n2) 
+                {
+                    root=root->right;
+                }
+                else if(root->data>n1&& root->data>n2) root=root->left;
+                else break;
             }
+            
+            return root;
         }
 
 };
