a98325c3973583e976c26d55a3b6a22d477ae3e0
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-07-18 11:13:34 +0530

    Time: 120 ms (86.20%), Space: 9 MB (70.38%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -14,7 +14,19 @@ public:
         return dp[n]= ans;
     }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
+        
+        for(int j=1;j<=n;j++)
+        {
+            int ans=INT_MAX;
+            for(int i=1;i*i<=n;i++)
+            {
+                if(j-i*i>=0)
+                ans=min(ans,1 + dp[j-i*i]); // 
+            }
+        
+            dp[j]= ans;
+        }
         return solve(n,dp);
     }
 };
