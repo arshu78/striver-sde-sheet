00fe82ae19a7487ddf122524d06e4669f81a6cff
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-08-02 19:40:05 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -5,19 +5,23 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
-	    int f(int i,int j,string& s1,string& s2,vector<vector<int>>& dp)
+	    int lcs(int i,int j,string &s1,string &s2,vector<vector<int>> &dp)
 	    {
 	        if(i<0 || j<0) return 0;
 	        
 	        if(dp[i][j]!=-1) return dp[i][j];
-	        if(s1[i]==s2[j] && i!=j) return 1 + f(i-1,j-1,s1,s2,dp);
-	        
-	        return dp[i][j]= max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+	        if(s1[i]==s2[j] && i!=j)
+	        {
+	            return dp[i][j]= 1 + lcs(i-1,j-1,s1,s2,dp);
+	        }
+	        else
+	        return dp[i][j]= max(lcs(i-1,j,s1,s2,dp),lcs(i,j-1,s1,s2,dp));
+	       
 	    }
 		int LongestRepeatingSubsequence(string str){
 		    int n=str.length();
 		    vector<vector<int>> dp(n,vector<int>(n,-1));
-		    return f(n-1,n-1,str,str,dp);
+		    return lcs(n-1,n-1,str,str,dp);
 		}
 
 };
