b991bd33c1c9c62b39283c4716e6a76f71273506
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-08-05 12:35:16 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (76.94%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -22,9 +22,29 @@ public:
         
             
     }
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int mincostTickets(vector<int>& days, vector<int>& cost) {
         int n=days.size();
-        vector<int> dp(n+1,-1);
-        return f(0,n,days,costs,dp);
+        vector<int> dp(n+1,INT_MAX);
+        
+        dp[n]=0;
+        
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            int op1=cost[0] + dp[ind+1];
+        
+        
+        int i;
+        for(i=ind;i<n && days[i] < days[ind] + 7;i++);
+        
+        int op2=cost[1] + dp[i];
+        
+         for(i=ind;i<n && days[i] < days[ind] + 30;i++);
+        
+        int op3=cost[2] + dp[i];
+        
+        
+        dp[ind]= min(op1,min(op2,op3));
+        }
+        return dp[0];
     }
 };
