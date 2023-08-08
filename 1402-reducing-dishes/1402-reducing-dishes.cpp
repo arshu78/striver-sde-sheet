ea16536d0d9baa74218dd95867a3c7a9d096b8cf
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-08-08 15:13:46 +0530

    Time: 32 ms (56.17%), Space: 20.9 MB (32.27%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -11,10 +11,21 @@ public:
         
         return dp[i][time]= max(include,exclude);
     }
-    int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(),satisfaction.end());
-        int n=satisfaction.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return f(satisfaction,0,0,dp);
+    int maxSatisfaction(vector<int>& s) {
+        sort(s.begin(),s.end());
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int time=i;time>=0;time--)
+            {
+                 int include = s[i] * (time+1) + dp[i+1][time+1];
+                int exclude=  0 + dp[i+1][time];
+
+                dp[i][time]= max(include,exclude);
+            }
+        }
+        return dp[0][0];
     }
 };
