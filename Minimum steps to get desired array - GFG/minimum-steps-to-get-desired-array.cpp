cfa68e1d5fc7eaabb88b8ce2675f31e1cd21bb5f
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-08-16 09:16:44 +0530

    Added solution - LeetHub

---
+++ Minimum steps to get desired array - GFG/minimum-steps-to-get-desired-array.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int countMinOperations(int arr[], int n) {
+        int ans=0;
+        while(1)
+        {
+            int zero=0;
+            
+            int i;
+            for(i=0;i<n;i++)
+            {
+                if(arr[i] % 2==1) break;
+                else if(arr[i]==0) zero++;
+            }
+            
+            if(zero==n) return ans;
+            if(i==n)
+            {
+                for(int i=0;i<n;i++)
+                {
+                    arr[i]=arr[i] / 2;
+                }
+                ans++;
+            }
+            
+            for(int j=i;j<n;j++)
+            {
+                if(arr[j] % 2==1)
+                {
+                    arr[j]--;
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countMinOperations(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
