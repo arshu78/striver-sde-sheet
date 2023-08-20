4f33515f646cfa6b78c70c6b1a1d7f4917e5ae8e
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-08-20 11:52:17 +0530

    Time: 545 ms (66.67%), Space: 131.8 MB (50.00%) - LeetHub

---
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int f(vector<vector<int>>& offers,vector<int> &dp,int n)
+    {
+        vector<int> e,s;
+        for(auto it :offers)
+        {
+            s.push_back(it[0]);
+            e.push_back(it[1]);
+        }
+        
+        for(int i=offers.size()-1;i>=0;i--)
+        {
+            int ind=upper_bound(s.begin(),s.end(),e[i])- s.begin();
+            dp[i]= max(offers[i][2] + dp[ind],dp[i+1]);
+        }
+        
+        return *max_element(dp.begin(),dp.end());
+    }
+    int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        sort(offers.begin(),offers.end());
+        vector<int> dp(offers.size()+1,0);
+        return f(offers,dp,n);
+    }
+};
