2243cd73f79fe4decf59e77af95c6e90d95bcee3
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-09-28 19:52:37 +0530

    Added solution - LeetHub

---
+++ Add Binary Strings - GFG/add-binary-strings.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	string addBinary(string a, string b)
+	{
+	     string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
+    }
+
+    reverse(begin(ans), end(ans));
+    i=0;
+    while(ans[i]!='1')
+    {
+        i++;
+    }
+    return ans.substr(i);
+	}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string A, B; cin >> A >> B;
+		Solution ob;
+		cout << ob.addBinary (A, B);
+		cout << "\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
