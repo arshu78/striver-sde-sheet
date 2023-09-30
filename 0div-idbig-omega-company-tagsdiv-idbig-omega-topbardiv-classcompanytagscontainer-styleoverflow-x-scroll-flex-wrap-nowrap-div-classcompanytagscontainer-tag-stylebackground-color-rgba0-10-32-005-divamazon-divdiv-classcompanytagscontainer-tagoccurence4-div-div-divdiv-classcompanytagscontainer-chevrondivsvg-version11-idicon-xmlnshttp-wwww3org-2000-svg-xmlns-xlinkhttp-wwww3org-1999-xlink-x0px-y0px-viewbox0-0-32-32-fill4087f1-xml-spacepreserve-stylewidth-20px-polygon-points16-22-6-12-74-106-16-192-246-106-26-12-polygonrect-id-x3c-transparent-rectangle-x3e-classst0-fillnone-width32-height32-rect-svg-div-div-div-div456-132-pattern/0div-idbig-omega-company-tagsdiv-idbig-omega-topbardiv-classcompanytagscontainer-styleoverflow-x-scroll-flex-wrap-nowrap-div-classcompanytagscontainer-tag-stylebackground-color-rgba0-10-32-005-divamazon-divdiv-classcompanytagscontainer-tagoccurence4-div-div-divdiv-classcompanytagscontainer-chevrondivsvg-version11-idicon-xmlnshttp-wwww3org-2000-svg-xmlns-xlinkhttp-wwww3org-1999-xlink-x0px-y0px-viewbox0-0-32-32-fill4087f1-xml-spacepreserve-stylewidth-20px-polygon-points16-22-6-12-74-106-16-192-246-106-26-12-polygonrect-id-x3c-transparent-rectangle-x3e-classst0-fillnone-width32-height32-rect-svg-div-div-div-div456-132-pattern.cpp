6dd5bf3294792eb2c4e046cdb45d508102bfa937
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-09-30 23:02:13 +0530

    Time: 65 ms (71.49%), Space: 48.9 MB (32.15%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        
+     int mini=INT_MIN;
+        stack<int> st;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(nums[i]<mini) return true;
+            
+            while(!st.empty() && nums[i]>st.top())
+            {
+                mini=st.top();
+                st.pop();
+            }
+            
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
