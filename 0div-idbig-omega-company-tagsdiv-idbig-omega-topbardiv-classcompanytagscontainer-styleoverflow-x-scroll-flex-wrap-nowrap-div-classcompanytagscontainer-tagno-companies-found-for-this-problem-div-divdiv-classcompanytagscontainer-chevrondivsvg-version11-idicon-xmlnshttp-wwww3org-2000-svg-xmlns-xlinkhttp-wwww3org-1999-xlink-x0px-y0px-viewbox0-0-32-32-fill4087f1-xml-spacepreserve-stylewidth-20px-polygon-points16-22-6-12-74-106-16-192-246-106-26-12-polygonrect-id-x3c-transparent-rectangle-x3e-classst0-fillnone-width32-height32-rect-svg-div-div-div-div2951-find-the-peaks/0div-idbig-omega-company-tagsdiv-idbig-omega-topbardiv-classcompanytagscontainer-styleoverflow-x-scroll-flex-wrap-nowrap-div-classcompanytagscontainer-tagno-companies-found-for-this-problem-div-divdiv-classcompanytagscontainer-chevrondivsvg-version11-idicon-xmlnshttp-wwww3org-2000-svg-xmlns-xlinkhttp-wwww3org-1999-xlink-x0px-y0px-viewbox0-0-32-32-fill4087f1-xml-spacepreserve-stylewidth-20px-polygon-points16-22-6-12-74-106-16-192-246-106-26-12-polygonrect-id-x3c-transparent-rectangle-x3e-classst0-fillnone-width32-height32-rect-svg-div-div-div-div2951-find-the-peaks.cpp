22fd198100e56874ffabdeff7f7e6b6c8f1b585a
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-12-09 12:06:54 +0530

    Time: 7 ms (52.46%), Space: 21 MB (63.78%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2951-find-the-peaks/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2951-find-the-peaks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& m) {
+        vector<int> ans;
+        for(int i=1;i<m.size()-1;i++)
+        {
+            if(m[i-1]<m[i] && m[i+1]<m[i]) ans.push_back(i);
+        }
+        return ans;
+    }
+};
