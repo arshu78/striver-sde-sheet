95277f8d61f3131d48e2cb718c3621412c9b05a6
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-12-24 20:41:07 +0530

    Time: 2 ms (63.59%), Space: 6.5 MB (43.86%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long int f(long long int n)
+    {
+        if(n==1) return 0;
+        if(n==0) return 1;
+        
+        if(n%2==0) return 1 + f(n/2);
+        return 1 + min(f(n-1),f(n+1));
+      
+    }
+    int integerReplacement(int n) {
+        return f(n);
+    }
+};
